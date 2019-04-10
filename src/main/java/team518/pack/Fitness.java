a5445cb5cab2b85fd01d8ce477fbddd6d9f85392
TuSiyu <tu.si@husky.neu.edu>
2019-04-10 11:03:38 -0400

    Fitness

---
+++ src/main/java/team518/pack/Fitness.java
@@ -0,0 +1,11 @@
+package team518.pack;
+
+public class Fitness {
+	public double fit(double x, double y, double X, double Y){
+		double fitness;
+		double x_d = X-x;
+		double y_d = Y-y;
+		fitness = Math.sqrt (Math.pow (x_d,2)+Math.pow (y_d,2));
+		return fitness;
+	}
+}
--- src/main/java/team518/pack/OutPutTest.java
+++ src/main/java/team518/pack/OutPutTest.java
@@ -11,10 +11,17 @@ public class OutPutTest {
 //        System.out.println(ind.getGene());
 //        System.out.println(ind.getxPosition());
 //        System.out.println(ind.getyPosition());
-        Galaxy galaxy = new Galaxy();
-        galaxy.createMine();
-        System.out.println(galaxy.getMine().getxPosition());
-        System.out.println(galaxy.getMine().getyPosition());
+
+       // Galaxy Test
+//        Galaxy galaxy = new Galaxy();
+//        galaxy.createMine();
+//        System.out.println(galaxy.getMine().getxPosition());
+//        System.out.println(galaxy.getMine().getyPosition());
+
+        //Fitness Test
+        Fitness fitness = new Fitness ();
+        double x =fitness.fit (0,0,12,5);
+        System.out.println (x);
     }
 
     public static void main (String[] arg) {
